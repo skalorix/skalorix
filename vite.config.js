e47c76cd9ac08a5bba5153fa5ba736d888ee6f51
skalorix <skalorix.work@gmail.com>
2026-09-14 03:20:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/',
+  
   plugins: [react()],
+
   build: {
     rolldownOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules/three')) return 'three';
-          if (id.includes('node_modules/@react-three')) return 'r3f';
-          if (id.includes('node_modules/gsap')) return 'gsap';
+          if (id.includes('node_modules/three')) return 'three'
+          if (id.includes('node_modules/@react-three')) return 'r3f'
+          if (id.includes('node_modules/gsap')) return 'gsap'
         },
       },
     },
